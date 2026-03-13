99a7835e0cc33f68377b87264d8492304bb77f92
Stellina Ao <stellinaxxao@gmail.com>
2026-03-13 15:19:21 -0700

    cleaned with pre-commit hooks - sa

--- scripts/gru_gs.py
+++ scripts/gru_gs.py
@@ -16,23 +16,20 @@ from gru.trainer import Trainer
 learner = "late"
 data = get_data(learner)
 
+
 # optuna
 def objective(trial):
     start = time.time()
-    # hparams 
+    # hparams
     lr = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
     batch_size = trial.suggest_categorical("batch_size", [16, 32, 64])
     hidden_size = trial.suggest_int("hidden_dim", 64, 256, step=64)
-    
-    config = {
-        'lr': lr,
-        'optimizer': optim.AdamW,
-        'criterion': nn.MSELoss
-    }
-    
+
+    config = {"lr": lr, "optimizer": optim.AdamW, "criterion": nn.MSELoss}
+
     print_every = 1
     max_epochs = 50
-    ckp_name = f'{learner}_combo-{trial.number}'
+    ckp_name = f"{learner}_combo-{trial.number}"
 
     # seed
     seed = 1234 + trial.number
@@ -41,25 +38,28 @@ def objective(trial):
     torch.manual_seed(seed)
 
     # instantiate model and trainer
-    gru = RateGRU(input_size=data['X_train'].shape[-1], 
-                  hidden_size=hidden_size, 
-                  output_size=data['Y_train'].shape[-1], 
-                  num_layers=1
-                )
-    
-    trainer = Trainer(gru, data,
-                    optim_config=config, 
-                    batch_size=batch_size,
-                    n_epochs=max_epochs, # really max epochs with pruning
-                    trial=trial,
-                    checkpoint_name=ckp_name, 
-                    print_every=print_every,
-                    verbose=True
-                    )
+    gru = RateGRU(
+        input_size=data["X_train"].shape[-1],
+        hidden_size=hidden_size,
+        output_size=data["Y_train"].shape[-1],
+        num_layers=1,
+    )
+
+    trainer = Trainer(
+        gru,
+        data,
+        optim_config=config,
+        batch_size=batch_size,
+        n_epochs=max_epochs,  # really max epochs with pruning
+        trial=trial,
+        checkpoint_name=ckp_name,
+        print_every=print_every,
+        verbose=True,
+    )
     # train and eval
-    trainer.train()  
+    trainer.train()
     r2_val = trainer.best_val_r2
-    
+
     end = time.time()
     print(f"Trial {trial.number} took {end - start:.2f} seconds")
 
@@ -70,16 +70,17 @@ def objective(trial):
 
     return r2_val
 
+
 # study
 study = optuna.create_study(
     direction="maximize",  # max val r2
     sampler=optuna.samplers.TPESampler(seed=1234),  # Bayesian opt
-    pruner=optuna.pruners.MedianPruner()  # optional early stopping
+    pruner=optuna.pruners.MedianPruner(),  # optional early stopping
 )
 
 # optimization
 n_combos = 36
-study.optimize(objective, n_trials=n_combos, n_jobs=1)  
+study.optimize(objective, n_trials=n_combos, n_jobs=1)
 
 # log
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -87,11 +88,7 @@ log_file = f"optuna_hpo_results_{timestamp}.json"
 
 results = []
 for t in study.trials:
-    results.append({
-        "params": t.params,
-        "value": t.value,
-        "state": str(t.state)
-    })
+    results.append({"params": t.params, "value": t.value, "state": str(t.state)})
 
 with open(log_file, "w") as f:
     json.dump(results, f, indent=2)
--- src/gru/trainer.py
+++ src/gru/trainer.py
@@ -1,57 +1,64 @@
-'''
+"""
 adapted from cs 231's solver class
-'''
+"""
+
 import numpy as np
 import pickle
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader, Dataset
+from torch.utils.data import TensorDataset, DataLoader
 from scipy.stats import pearsonr
-from .model import *
 import copy
 import optuna
 import os
 
+
 class Trainer:
     def __init__(self, model, data, **kwargs):
-        '''
+        """
         optim_config expects:
         - lr
         - optimizer (class, not object)
         - criterion (class, not object)
-        '''
+        """
         self.model = model
-        
+
         # kwargs
-        self.optim_config = kwargs.pop('optim_config', {})
-        self.lr_decay     = kwargs.pop('lr_decay', 1.0)
-        self.batch_size   = kwargs.pop('batch_size', 1)
-        self.n_epochs     = kwargs.pop('n_epochs', 10)
-        self.patience     = kwargs.pop('patience', 1e-3)
-        
-        self.trial = kwargs.pop('trial', None)
-        
-        self.checkpoint_name = kwargs.pop('checkpoint_name', None)
-        self.print_every     = kwargs.pop('print_every', 10)
-        self.verbose         = kwargs.pop('verbose', True)
-        
-        self.lr        = self.optim_config['lr']
-        self.optimizer = self.optim_config['optimizer'](params=self.model.parameters(), lr=self.lr)
-        self.criterion = self.optim_config['criterion']()
-        
+        self.optim_config = kwargs.pop("optim_config", {})
+        self.lr_decay = kwargs.pop("lr_decay", 1.0)
+        self.batch_size = kwargs.pop("batch_size", 1)
+        self.n_epochs = kwargs.pop("n_epochs", 10)
+        self.patience = kwargs.pop("patience", 1e-3)
+
+        self.trial = kwargs.pop("trial", None)
+
+        self.checkpoint_name = kwargs.pop("checkpoint_name", None)
+        self.print_every = kwargs.pop("print_every", 10)
+        self.verbose = kwargs.pop("verbose", True)
+
+        self.lr = self.optim_config["lr"]
+        self.optimizer = self.optim_config["optimizer"](
+            params=self.model.parameters(), lr=self.lr
+        )
+        self.criterion = self.optim_config["criterion"]()
+
         # throw error if there are extra kw args
         if len(kwargs) > 0:
-            extra_kwargs = ', '.join('"%s' % k for k in list(kwargs.keys()))
-            raise ValueError('Extra arguments %s' % extra_kwargs)
-        
+            extra_kwargs = ", ".join('"%s' % k for k in list(kwargs.keys()))
+            raise ValueError("Extra arguments %s" % extra_kwargs)
+
         # dataloaders
-        self.train_loader = DataLoader(TensorDataset(data['X_train'], data['Y_train']), batch_size=self.batch_size, shuffle=False)
-        self.val_loader   = DataLoader(TensorDataset(data['X_val'], data['Y_val']), batch_size=self.batch_size)
-        
+        self.train_loader = DataLoader(
+            TensorDataset(data["X_train"], data["Y_train"]),
+            batch_size=self.batch_size,
+            shuffle=False,
+        )
+        self.val_loader = DataLoader(
+            TensorDataset(data["X_val"], data["Y_val"]), batch_size=self.batch_size
+        )
+
         # book-keeping
         self._reset()
-        
+
     def _reset(self):
         self.epoch = 0
         self.best_val_r2 = -np.inf
@@ -59,86 +66,85 @@ class Trainer:
         self.loss_history = []
         self.train_r2_history = []
         self.val_r2_history = []
-        
+
         self.optim_configs = {}
         for p in self.model.parameters():
             d = {k: v for k, v in self.optim_config.items()}
             self.optim_configs[p] = d
-            
+
     def _save_checkpoint(self):
-        if self.checkpoint_name is None: 
+        if self.checkpoint_name is None:
             return
-        
+
         checkpoint = {
-            'model': self.model,
-            'lr_decay': self.lr_decay,
-            'optim_config': self.optim_config,
-            'batch_size': self.batch_size,
-            'epoch': self.epoch,
-            'loss_history': self.loss_history,
-            'train_r2_history': self.train_r2_history,
-            'val_r2_history': self.val_r2_history,
-            'best_val_r2': self.best_val_r2,
-            'best_params': self.best_params
+            "model": self.model,
+            "lr_decay": self.lr_decay,
+            "optim_config": self.optim_config,
+            "batch_size": self.batch_size,
+            "epoch": self.epoch,
+            "loss_history": self.loss_history,
+            "train_r2_history": self.train_r2_history,
+            "val_r2_history": self.val_r2_history,
+            "best_val_r2": self.best_val_r2,
+            "best_params": self.best_params,
         }
-        filename = 'checkpoints/%s_epoch_%d.pkl' % (self.checkpoint_name, self.epoch)
+        filename = "checkpoints/%s_epoch_%d.pkl" % (self.checkpoint_name, self.epoch)
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         if self.verbose:
             print("saving checkpoint to '%s'" % filename)
-        with open(filename, 'wb') as f:
+        with open(filename, "wb") as f:
             pickle.dump(checkpoint, f)
-    
+
     def check_r2(self, data_loader, chunk_size=100):
         self.model.eval()
-        
+
         y_eval = []
         y_pred = []
-        
+
         with torch.no_grad():
             for x_batch, y_batch in data_loader:
                 device = next(self.model.parameters()).device
                 x_batch = x_batch.to(device)
                 y_batch = y_batch.to(device)
-                
+
                 out, _ = self.model(x_batch, h=None)
-                
+
                 y_eval.append(y_batch.cpu())
                 y_pred.append(out.cpu())
-        
+
         y_eval = torch.cat(y_eval, dim=0)
         y_pred = torch.cat(y_pred, dim=0)
-        
+
         r2 = pearsonr(y_eval.flatten().numpy(), y_pred.flatten().numpy()).statistic
-        
+
         return r2
-        
-        
+
     def train(self):
         for epoch in range(self.n_epochs):
             self.epoch = epoch
             self.model.train()
-            
+
             # reset h_prev every epoch (torch handles h=None)
-            h = None 
-            
+            h = None
+
             for i, (x_batch, y_batch) in enumerate(self.train_loader):
                 device = next(self.model.parameters()).device
                 x_batch = x_batch.to(device)
                 y_batch = y_batch.to(device)
-                
-                self.optimizer.zero_grad() # reset grads
-                
+
+                self.optimizer.zero_grad()  # reset grads
+
                 if h is not None and h.size(1) != x_batch.size(0):
-                    h = h[:,:x_batch.size(0),:].contiguous()
-                
-                out, h = self.model(x_batch, h) # forward pass
-                loss = self.criterion(out, y_batch) # loss
-                loss.backward() # backprop
-                self.optimizer.step() # update params
-                
-                h = h.detach() # prevent h from spilling over
-                
-                if i==len(self.train_loader)-1:
+                    h = h[:, : x_batch.size(0), :].contiguous()
+
+                out, h = self.model(x_batch, h)  # forward pass
+                loss = self.criterion(out, y_batch)  # loss
+                loss.backward()  # backprop
+                self.optimizer.step()  # update params
+
+                h = h.detach()  # prevent h from spilling over
+
+                if i == len(self.train_loader) - 1:
                     # save loss first
                     self.model.eval()
                     with torch.no_grad():
@@ -146,19 +152,19 @@ class Trainer:
                             device = next(self.model.parameters()).device
                             x_batch = x_batch.to(device)
                             y_batch = y_batch.to(device)
-                            
+
                             out, _ = self.model(x_batch, h=None)
                             val_loss = self.criterion(out, y_batch)
                             self.loss_history.append(val_loss.item())
-                    
+
                     # then check r2
                     train_r2 = self.check_r2(self.train_loader)
-                    val_r2   = self.check_r2(self.val_loader)
-                    
+                    val_r2 = self.check_r2(self.val_loader)
+
                     self.train_r2_history.append(train_r2)
                     self.val_r2_history.append(val_r2)
                     self._save_checkpoint()
-                    
+
                     # optuna
                     if self.trial is not None:
                         self.trial.report(val_r2, step=epoch)
@@ -167,23 +173,14 @@ class Trainer:
                     if val_r2 > self.best_val_r2:
                         self.best_val_r2 = val_r2
                         self.best_params = copy.deepcopy(self.model.state_dict())
-            
+
             # done with batches for one epoch
             for g in self.optimizer.param_groups:
-                g['lr'] *= self.lr_decay
-                 
-            if self.verbose and (epoch+1) % self.print_every == 0:
-                print(f"(Epoch {self.epoch} / {self.n_epochs}) train r2: {train_r2:.3f}; val r2: {val_r2:.3f}; loss: {loss.item():.3f}")
-        
-        self.model.load_state_dict(self.best_params)
-        
-# batch_size = 32
-# n_tbins = 10 # 1 sec, 0.1 sec bins
-# input_size = 50 # nfeatures
-# hidden_size = 100
-# x = torch.randn(batch_size, n_tbins, input_size)
-# h_prev = torch.zeros(batch_size, hidden_size) # not updating h_prev...
-# y = torch.randn(batch_size, n_tbins, hidden_size)
+                g["lr"] *= self.lr_decay
 
-# gru = RateGRU(input_size, hidden_size)
-# gru = train_model(gru, x, y, h_prev)
+            if self.verbose and (epoch + 1) % self.print_every == 0:
+                print(
+                    f"(Epoch {self.epoch} / {self.n_epochs}) train r2: {train_r2:.3f}; val r2: {val_r2:.3f}; loss: {loss.item():.3f}"
+                )
+
+        self.model.load_state_dict(self.best_params)
